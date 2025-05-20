0fd48412a704b07dca80e60daec2b37526393a9e
mazode <mrmaazkhan191@gmail.com>
2025-05-20 22:18:49 +0300

    designed chat details section

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useRef, useState } from 'react';
 import './chat.css'
+import EmojiPicker from 'emoji-picker-react'
+
 
 const Chat = () => {
+  const [pickEmoji, setPickEmoji] = useState(false);
+  const [text, setText] = useState("");
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({behavior:'smooth'})
+  }, [])
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+  }
+
   return (
     <div className='chat'>
       <div className="top">
@@ -17,13 +33,70 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
-      <div className="bottom">
-        <div className="icons"></div>
-        <input type="text" placeholder='Type a message...' />
-        <div className="emoji">
-          <img src="./emoji.png" alt="" />
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Random text for message</p>
+            <span>1 min ago</span>
+          </div>
         </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Random text for message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Random text for message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Random text for message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Random text for message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="https://images.unsplash.com/photo-1747330721960-681faaf7a92a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <p>Random text for message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input 
+        type="text" 
+        placeholder='Type a message...' 
+        value={text}
+        onChange={e => setText(e.target.value)} 
+        />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" 
+          onClick={() => setPickEmoji((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={pickEmoji} onEmojiClick={handleEmoji}/>
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
       </div>
     </div>
   )
--- src/components/details/Detail.jsx
+++ src/components/details/Detail.jsx
@@ -2,7 +2,57 @@ import './detail.css'
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Mazode</h2>
+        <p>Random user description goes here.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://images.unsplash.com/photo-1747633126452-dee49902fc6e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+              <span>photo.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://images.unsplash.com/photo-1747633126452-dee49902fc6e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+              <span>photo.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className='logout'>Logout</button>
+      </div>
+    </div>
   )
 }
 export default Detail
